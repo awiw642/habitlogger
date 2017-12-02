9cb298d9cddbd92c4eff1eced235862afa206318
Aprianto Wiwarsono <awiwarsono@gmail.com>
2017-12-03 10:04:25 +1300

    Styling Login

--- client/src/LoggerCalendar.jsx
+++ client/src/LoggerCalendar.jsx
@@ -2,12 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Calendar, CalendarControls } from 'react-yearly-calendar';
 
-const LoggerCalendar = () => (
-  <div>
-    <CalendarControls year={2017} />
-    <Calendar />
-  </div>
-);
+class LoggerCalendar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentDate: new Date(),
+      showWeekSeparators: false,
+    };
+  }
+  render() {
+    return (
+      <div id="calendar">
+        <CalendarControls year={2017} selectedDay={this.state.currentDate} />
+        <Calendar showWeekSeparators={this.state.showWeekSeparators} />
+      </div>
+    );
+  }
+}
 
 Calendar.propTypes = {};
 
